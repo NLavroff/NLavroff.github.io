11f931ee43533a480fa1e47a3b52e03bb0c0f300
Nathalie Lavroff <nathalielavroff@gmail.com>
2022-01-09 12:25:17 +0100

    form is working

--- index.js
+++ index.js
@@ -1,51 +1,69 @@
-'use strict';
+"use strict";
 
 const cards = document.querySelector(".cards");
 
 const projects = [
-    {
-        name: "Malice Shop",
-        picture: "img/projet2.png"
-    },
-    {
-        name: "DonkeyEvent",
-        picture: "img/projet3.png"
-    }
+  {
+    name: "Vivo Green",
+    picture: "img/projet1.png",
+    link: "https://www.vivo-green.fr/",
+  },
+  {
+    name: "Malice Shop",
+    picture: "img/projet2.png",
+    link: "https://malice-shop.fr/",
+  },
+  {
+    name: "DonkeyEvent",
+    picture: "img/projet3.png",
+    link: "https://www.vivo-green.fr/",
+  },
 ];
 
-function createCard(title, imageUrl) {
-    const card = document.createElement("div");
-    card.classList.add("card");
-    cards.appendChild(card);
+function createCard(title, imageUrl, link) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  cards.appendChild(card);
 
-    const cardHeader = document.createElement("div");
-    cardHeader.classList.add("card-header");
-    card.appendChild(cardHeader);
+  const cardHeader = document.createElement("div");
+  cardHeader.classList.add("card-header");
+  card.appendChild(cardHeader);
 
-    const cardImg = document.createElement("div");
-    cardImg.style.backgroundImage = `url(${imageUrl})`;
-    cardImg.classList.add("card-img");
-    cardHeader.appendChild(cardImg);
+  const cardImg = document.createElement("div");
+  cardImg.style.backgroundImage = `url(${imageUrl})`;
+  cardImg.classList.add("card-img");
+  cardHeader.appendChild(cardImg);
 
-    const cardBody = document.createElement("div");
-    cardBody.classList.add("card-body");
-    card.appendChild(cardBody);
-  
-    const cardTitle = document.createElement("h2");
-    cardTitle.classList.add("card-title");
-    cardTitle.innerHTML = title;
-    cardBody.appendChild(cardTitle);
-  
-    const cardButton = document.createElement("button");
-    cardButton.classList.add("card-button");
-    cardButton.innerHTML = "Voir plus de détails";
-    cardBody.appendChild(cardButton);
-  }
-  
-    for (let i = 0; i < projects.length; i++) {
-      createCard(projects[i].name, projects[i].picture);
-    }
+  const cardBody = document.createElement("div");
+  cardBody.classList.add("card-body");
+  card.appendChild(cardBody);
 
-    function clickOnCardImg() {
-        
-    }
+  const cardTitle = document.createElement("h2");
+  cardTitle.classList.add("card-title");
+  cardTitle.innerHTML = title;
+  cardBody.appendChild(cardTitle);
+
+  // const cardLink = document.createElement("a");
+  // cardLink.classList.add("card-link");
+  // cardLink.href = link;
+  // cardLink.innerHTML = "Le visiter";
+  // cardBody.appendChild(cardLink);
+
+  const cardButton = document.createElement("button");
+  cardButton.classList.add("btn");
+  cardButton.classList.add("btn-outline-primary");
+  cardButton.onclick = visitSite(link);
+  // cardButton.disabled;
+  //cardButton.href = link;
+  cardButton.innerHTML = "Le visiter";
+  cardBody.appendChild(cardButton);
+}
+
+function visitSite(link) {
+  //window.open(link);
+  console.log("tamere");
+}
+
+for (let i = 0; i < projects.length; i++) {
+  createCard(projects[i].name, projects[i].picture, projects[i].link);
+}
--- mail.php
+++ mail.php
@@ -1,11 +1,11 @@
 <?php
-
-    if (isset($_POST['message'])) {
+    if (isset($_POST['email'])) {
         $position_arobase = strpos($_POST['email'], '@');
         if ($position_arobase === false)
             echo '<p>Votre email doit comporter un arobase.</p>';
         else {
             $send = mail('nathalielavroff@gmail.com', 'Envoi depuis la page Contact', $_POST['message'], 'From: ' . $_POST['email']);
+            echo $send;
             if($send)
                 echo '<p>Votre message a été envoyé.</p>';
             else
