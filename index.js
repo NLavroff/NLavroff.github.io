fbcca2dd8a853b297488a35de6437269e7937d7f
Nathalie Lavroff <nathalielavroff@gmail.com>
2022-01-23 17:28:15 +0100

    added back to top button

--- index.js
+++ index.js
@@ -98,3 +98,24 @@ const closeModal = (modal) => {
   document.removeEventListener("keydown", escClose);
   document.body.removeChild(document.getElementById("modal-overlay"));
 };
+
+const showOnPx = 100;
+const backToTopButton = document.querySelector(".back-to-top")
+
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+document.addEventListener("scroll", () => {
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden")
+  } else {
+    backToTopButton.classList.add("hidden")
+  }
+});
+
+const goToTop = () => {
+  document.body.scrollIntoView();
+};
+
+backToTopButton.addEventListener("click", goToTop)
